f8d143bb29422c17ba913ea07cf84830267b82ef
NotRealAI <77367116+NotRealAI@users.noreply.github.com>
2021-09-16 01:13:41 -0700

    Update PostCard.vue

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -5,12 +5,16 @@
     </div>
     <div class="post-card__content">
       <h2 class="post-card__title" v-html="post.title" />
+      
       <p class="post-card__description" v-html="post.description" />
-
+      <!--
       <PostMeta class="post-card__meta" :post="post" />
       <PostTags class="post-card__tags" :post="post" />
-
       <g-link class="post-card__link" :to="post.path">Link</g-link>
+      -->
+      
+      <g-link class="post-card__link" :to="post.external">Link</g-link>
+      
     </div>
   </div>
 </template>
