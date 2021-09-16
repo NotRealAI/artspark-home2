588c3ead921ebf2c8d236c76e3712f09ec3fe1ed
NotRealAI <77367116+NotRealAI@users.noreply.github.com>
2021-09-16 01:23:20 -0700

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,9 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: 'ArtSpark',
+  siteDescription: '',
+  twitter: 'artspark_io',
 
   templates: {
     Post: '/:title',
