8b924ac6466de634bb8cc30c60abbf5f4bd88d02
NotRealAI <77367116+NotRealAI@users.noreply.github.com>
2021-09-16 01:28:03 -0700

    Update Author.vue

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -5,11 +5,11 @@
 			{{ $static.metadata.siteName }}
 		</h1>
 
-		<!--
+		
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.
+			
 		</p>
-		-->
+		
 
 		<p class="author__links">
 			<a href="//twitter.com/{{ $static.metadata.twitter }}">Twitter</a>
